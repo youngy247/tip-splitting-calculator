d5c9bccee6cdd75bb33c035f9066e223dafb1e0b
Adam Young <adamyoungy678@gmail.com>
2023-04-28 11:11:23 +0100

    added number of people handler

--- src/components/PeopleNumber/index.js
+++ src/components/PeopleNumber/index.js
@@ -1,10 +1,22 @@
+import {useState} from "react";
+
 const PeopleNumber = () => {
+
+    const [numPeople, setNumPeople] = useState('')
+
     return (
-        <div>
-            <div>PeopleNumber</div>
-            <input type='number'/>
+        <div className="form-group">
+            <label htmlFor="numPeople">Number of People</label>
+            <input
+                type="number"
+                className="form-control"
+                id="numPeople"
+                value={numPeople}
+                onChange={(e) => setNumPeople(e.target.value)}
+            />
         </div>
     )
 }
+
 
 export default PeopleNumber
