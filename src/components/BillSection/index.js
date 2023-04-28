b383cab7806aba1b05a60feb5cdafef1f3f700c0
Adam Young <adamyoungy678@gmail.com>
2023-04-28 11:14:24 +0100

    added bill amount handler

--- src/components/BillSection/index.js
+++ src/components/BillSection/index.js
@@ -1,9 +1,24 @@
+import {useState} from "react";
+
 const BillSection = () => {
+
+    const [billAmount, setBillAmount] = useState('')
+
     return (
         <div>
-        <div>Bill Section</div>
-            <input type='number'/>
+            <h1>Tip Calculator</h1>
+            <div className="form-group">
+                <label htmlFor="billAmount">Bill Amount</label>
+                <input
+                    type="number"
+                    className="form-control"
+                    id="billAmount"
+                    value={billAmount}
+                    onChange={(e) => setBillAmount(e.target.value)}
+                />
+            </div>
         </div>
+
     )
 }
 
