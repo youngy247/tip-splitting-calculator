a1d74d78be83c6a81e2aed17901e4d507004bf4c
Adam Young <adamyoungy678@gmail.com>
2023-04-28 11:05:14 +0100

    added select tip functionality

--- src/components/TipSelect/index.js
+++ src/components/TipSelect/index.js
@@ -1,20 +1,62 @@
+import { useEffect, useState } from "react";
+
 const TipSelect = () => {
+    const [tip, setTip] = useState(0);
+
+    useEffect(() => {
+        setTip(0);
+    }, []);
+
+    const handleTipClick = (percentage) => {
+        setTip(percentage);
+    };
+
     return (
         <div>
             <div>Select Tip</div>
             <div className="row">
-
-            <button className="btn btn-secondary col-4">5%</button>
-            <button className="btn btn-secondary col-4">10%</button>
-            <button className="btn btn-secondary col-4">15%</button>
+                <button
+                    className="btn btn-secondary col-4"
+                    onClick={() => handleTipClick(5)}
+                >
+                    5%
+                </button>
+                <button
+                    className="btn btn-secondary col-4"
+                    onClick={() => handleTipClick(10)}
+                >
+                    10%
+                </button>
+                <button
+                    className="btn btn-secondary col-4"
+                    onClick={() => handleTipClick(15)}
+                >
+                    15%
+                </button>
             </div>
             <div className="row">
-            <button className="btn btn-secondary col-4">25%</button>
-            <button className="btn btn-secondary col-4">50%</button>
-            <input type="number" className="col-4"/>
+                <button
+                    className="btn btn-secondary col-4"
+                    onClick={() => handleTipClick(25)}
+                >
+                    25%
+                </button>
+                <button
+                    className="btn btn-secondary col-4"
+                    onClick={() => handleTipClick(50)}
+                >
+                    50%
+                </button>
+                <input
+                    type="number"
+                    className="col-4"
+                    onChange={(e) => setTip(e.target.value)}
+                    value = {tip}
+                    placeholder="Custom"
+                />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TipSelect
+export default TipSelect;
