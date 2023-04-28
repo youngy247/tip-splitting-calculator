ca1db644e2fdb89720415a198bf142c5b80a8b65
Adam Young <adamyoungy678@gmail.com>
2023-04-28 11:25:30 +0100

    added tip ammount and total amount variables and displayed them

--- src/components/ResultSection/index.js
+++ src/components/ResultSection/index.js
@@ -1,16 +1,21 @@
+import {useState} from "react";
 import Button from "../Button";
 
 const ResultSection = () => {
+
+    const [tipAmount, setTipAmount] = useState(0)
+    const [totalAmount, setTotalAmount] = useState(0)
+
     return (
         <div className="d-flex flex-column justify-content-between">
             <div>
                 <div className="row">
                     <div className="col-6">Tip Amount</div>
-                    <div className="col-6">$0.00</div>
+                    <div className="col-6">${tipAmount}</div>
                 </div>
                 <div className="row">
                     <div className="col-6">Total</div>
-                    <div className="col-6">$0.00</div>
+                    <div className="col-6">${totalAmount}</div>
                 </div>
             </div>
             <div className="align-bottom">
